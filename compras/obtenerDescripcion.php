849c3c8b982641851a981432f0477071c2bcbd1e
AndresAlmi <107308520+AndresAlmi@users.noreply.github.com>
2022-06-13 10:24:04 -0300

    Delete compras directory

--- compras/obtenerDescripcion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "../../class/repuestos.php";
-$id = $_GET["codigo"];
-$descripcion = "";
-$repuesto = repuestos::obtenerPorIdRepuesto($id);
-$descripcion = $repuesto->getDescripcion();
-echo $descripcion;
-?>
